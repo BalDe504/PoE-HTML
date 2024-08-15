80eed06e9e29b9bf611f0d597479d115396b48b2
BalDe504 <80625247+BalDe504@users.noreply.github.com>
2024-08-15 17:46:18 +0200

    Added Modal Menu
    Added fully functiona modal menu to the project

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-document.write("JS test");
+var menu_modal = document.getElementById("menu-modal");
+var menu_button = document.getElementById("menu-button");
+var close_menu = document.getElementsByClassName("close-menu")[0];
+
+menu_button.onclick = function(){
+    menu_modal.style.display = "block";
+}
+
+close_menu.onclick = function(){
+    menu_modal.style.display = "none";
+}
+
+window.onclick = function(event){
+    if(event.target == menu_modal){
+        menu_modal.style.display = "none";
+    }
+}
+
