6ac8b7e51f0f38300aa53d4f3b1bda6b1b666b3e
BalDe504 <80625247+BalDe504@users.noreply.github.com>
2024-08-21 22:13:49 +0200

    Added Equipment panel
    Added eq

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ var menu_modal = document.getElementById("menu-modal");
 var menu_button = document.getElementById("menu-button");
 var close_menu = document.getElementsByClassName("close-menu")[0];
 
+var eq = document.getElementById("eq1");
+var eq_open = document.getElementById("eq_open");
+
 menu_button.onclick = function(){
     menu_modal.style.display = "block";
 }
@@ -10,9 +13,26 @@ close_menu.onclick = function(){
     menu_modal.style.display = "none";
 }
 
+eq_open.onclick = function(){
+
+    if(eq.style.display == "none"){
+        eq.style.display = "flex";
+    }
+    else{
+        eq.style.display = "none";
+    }
+    
+}
+
 window.onclick = function(event){
     if(event.target == menu_modal){
         menu_modal.style.display = "none";
     }
+
+    else if(event.target== eq){
+        eq.style.display = "none";
+    }
 }
+
+
 
